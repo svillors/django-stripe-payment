04805ee96caa8d23c02b6539068da5464ed668b8
svillors <tntzator9@gmail.com>
2026-04-28 20:09:52 +0200

    Create Item model and add to admin

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.TextChoices):
+    USD = 'usd', 'USD'
+    EUR = 'eur', 'EUR'
+
+
+class Item(models.Model):
+    name = models.CharField('Name', max_length=100)
+    description = models.TextField('Description', blank=True)
+    price = models.DecimalField('Price', max_digits=10, decimal_places=2)
+    currency = models.CharField(
+        'Currency',
+        max_length=3,
+        choices=Currency.choices,
+        default=Currency.USD,
+    )
+
+    def __str__(self):
+        return self.name
